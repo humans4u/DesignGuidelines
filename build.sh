c0112729836e887b2d25f47e2c299f92b79a5ff1
as43z <albertsigmaeng@gmail.com>
2024-02-15 20:10:24 +0100

    Rename library of CSS styles

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 EXTENSIONS=${EXTENSIONS-""}
-FLAGS=${FLAGS-"--from markdown${EXTENSIONS} --to html --css css/definition.css --css css/minimal.styles.css --metadata-file metadata.yml --standalone --verbose --pdf-engine prince "}
+FLAGS=${FLAGS-"--from markdown${EXTENSIONS} --to html --css css/_libstylesh4u.css --css css/libstylesh4u.css --metadata-file metadata.yml --standalone --verbose --pdf-engine prince "}
 # --from            specify the format of the input data
 # --to              specify the format of the output data
 # --css             include styling
